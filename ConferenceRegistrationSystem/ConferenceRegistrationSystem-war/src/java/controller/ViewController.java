10391afa13c9a38aef5c11e34675cbcb07b5528e
smahlke <smahlke@me.com>
2016-03-11 21:52:10 +0100

    Enh: Erste Gehversuche mit Dialogen

--- ConferenceRegistrationSystem/ConferenceRegistrationSystem-war/src/java/controller/ViewController.java
+++ ConferenceRegistrationSystem/ConferenceRegistrationSystem-war/src/java/controller/ViewController.java
@@ -7,27 +7,47 @@ package controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
 
 /**
  *
  * @author sebastianmahlke
  */
 @ManagedBean(name = "viewController")
+@SessionScoped
 public class ViewController {
     
     
-        /**
+    /**
      * Views
      */
     public void viewConferenceCreation() {
         Map<String, Object> options = new HashMap<>();
         options.put("resizable", false);
+        options.put("modal", true);
 
-        RequestContext.getCurrentInstance().openDialog("conference-modal", options, null);
-//        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "What we do in life", "Echoes in eternity.");
-//         
-//        RequestContext.getCurrentInstance().showMessageInDialog(message);
+        RequestContext.getCurrentInstance().openDialog("conference/workflow/conference", options, null);
+    }
+    
+    public void closeDialog() {
+        RequestContext.getCurrentInstance().closeDialog(null);
+    }
+    
+    public void saveDialog() {
+        RequestContext.getCurrentInstance().closeDialog(null);
+    }
+    
+    public void onConferenceSave(SelectEvent event) {
+        
+        Object obj = event.getObject();
+        System.out.println(obj);
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Car Selected", obj.toString());
+         
+        FacesContext.getCurrentInstance().addMessage(null, message);
     }
 }
